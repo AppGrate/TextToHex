9182e0fd9eecc3dc6fe70db65bce4687fa7bf8b9
appgrate <appgrate@outlook.com>
2020-10-12 12:31:49 -0400

    Done

---
+++ TextToHex/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TextToHex
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void English_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox txtBox)
+            {
+                var n = from t in txtBox.Text
+                        select ((int)t).ToString("X2");
+                HexBox.Text = string.Join(" ", n);
+            }
+        }
+    }
+}
